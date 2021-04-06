4f5ec33fd25901e2400da772b77d9eb8ecabcff9
Dheo Gildas Varian <dheogildasvarian@gmail.com>
2021-04-06 11:59:25 +0700

    WelcomePage layout done

---
+++ Pile/Intro/Welcome Page/WelcomePageViewController.swift
@@ -0,0 +1,107 @@
+//
+//  WelcomePageViewController.swift
+//  Pile
+//
+//  Created by Dheo Gildas Varian on 05/04/21.
+//
+
+import UIKit
+
+class WelcomePageViewController: UIPageViewController, UIPageViewControllerDelegate, UIPageViewControllerDataSource {
+    
+    var pageControl = UIPageControl()
+    
+    lazy var orderedViewControllers: [UIViewController] = {
+            return [self.newVc(viewController: "introPage1"),
+                    self.newVc(viewController: "introPage2"),
+                    self.newVc(viewController: "introPage3")]
+        }()
+    
+    //Function Data Source
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        
+        guard let viewControllerIndex = orderedViewControllers.index(of: viewController)
+        else {
+            return nil
+        }
+        
+        let previousIndex = viewControllerIndex - 1
+        
+        guard previousIndex >= 0 else {
+                    return nil
+                }
+        
+        guard orderedViewControllers.count > previousIndex else {
+                    return nil
+                }
+                
+        return orderedViewControllers[previousIndex]
+    }
+    
+    //Function Data Source
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        
+        guard let viewControllerIndex = orderedViewControllers.index(of: viewController) else {
+            return nil
+        }
+        
+        let nextIndex = viewControllerIndex + 1
+        let orderedViewControllersCount = orderedViewControllers.count
+        
+        guard orderedViewControllersCount != nextIndex else {
+            return nil
+        }
+        
+        guard orderedViewControllersCount > nextIndex else {
+            return nil
+        }
+        
+        return orderedViewControllers[nextIndex]
+    }
+    
+    //Function Delegate
+    func pageViewController(_ pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
+            let pageContentViewController = pageViewController.viewControllers![0]
+            self.pageControl.currentPage = orderedViewControllers.index(of: pageContentViewController)!
+        }
+    
+    
+    func newVc(viewController: String) -> UIViewController {
+            return UIStoryboard(name: "WelcomePage", bundle: nil).instantiateViewController(withIdentifier: viewController)
+        }
+        
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.dataSource = self
+
+        if let firstViewController = orderedViewControllers.first {
+                setViewControllers([firstViewController],
+                direction: .forward,
+                animated: true,
+                completion: nil)
+        }
+        
+        
+        self.delegate = self
+        configurePageControl()
+        
+        
+    }
+    
+    func configurePageControl() {
+            pageControl = UIPageControl(frame: CGRect(x: 0,y: UIScreen.main.bounds.maxY - 50,width: UIScreen.main.bounds.width,height: 50))
+            self.pageControl.numberOfPages = orderedViewControllers.count
+            self.pageControl.currentPage = 0
+            self.pageControl.tintColor = #colorLiteral(red: 0.3243525326, green: 0.5673766136, blue: 0.4923315048, alpha: 1)
+            self.pageControl.pageIndicatorTintColor = UIColor.lightGray
+            self.pageControl.currentPageIndicatorTintColor = #colorLiteral(red: 0.3243525326, green: 0.5673766136, blue: 0.4923315048, alpha: 1)
+            self.view.addSubview(pageControl)
+    }
+    
+    
+
+}
